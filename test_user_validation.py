8634fa4d66fe7f48bda0408b5811e25b223142a9
Mahmoud1102004 <164413054+Mahmoud1102004@users.noreply.github.com>
2025-10-24 22:35:48 +0300

    Update test_user_validation.py

--- test_user_validation.py
+++ test_user_validation.py
@@ -1,132 +1,127 @@
 import unittest
-from user_validation import UserValidation
+from user_validation import UserValidation as UV
 
 
-class UserValidationTests(unittest.TestCase):
+class TestUserValidation(unittest.TestCase):
+    def test_valid_email(self):
+        self.assertTrue(UV.validate_email("user@example.com"))
 
-    # ---------------- Email Tests ----------------
-    def test_email_valid(self):
-        self.assertTrue(UserValidation.validate_email("user@example.com"))
+    def test_email_without_at(self):
+        self.assertFalse(UV.validate_email("userexample.com"))
 
-    def test_email_missing_at_symbol(self):
-        self.assertFalse(UserValidation.validate_email("userexample.com"))
+    def test_email_no_domain(self):
+        self.assertFalse(UV.validate_email("user@"))
 
-    def test_email_missing_domain(self):
-        self.assertFalse(UserValidation.validate_email("user@"))
-
-    def test_email_invalid_tld(self):
-        self.assertFalse(UserValidation.validate_email("user@mail.c"))
+    def test_email_bad_tld(self):
+        self.assertFalse(UV.validate_email("user@mail.c"))
 
     def test_email_with_subdomain(self):
-        self.assertTrue(UserValidation.validate_email("user@mail.company.com"))
+        self.assertTrue(UV.validate_email("person@mail.company.com"))
 
-    def test_email_with_special_characters(self):
-        self.assertTrue(UserValidation.validate_email("ramy.gomaa_21@mail.co"))
+    def test_email_with_symbols(self):
+        self.assertTrue(UV.validate_email("ahmed.ali_99@mail.co"))
 
-    def test_email_uppercase(self):
-        self.assertTrue(UserValidation.validate_email("USER@MAIL.COM"))
+    def test_email_uppercase_letters(self):
+        self.assertTrue(UV.validate_email("USER@MAIL.COM"))
 
-    def test_email_with_space(self):
-        self.assertFalse(UserValidation.validate_email("user name@mail.com"))
+    def test_email_contains_space(self):
+        self.assertFalse(UV.validate_email("user name@mail.com"))
 
-    def test_email_empty(self):
-        self.assertFalse(UserValidation.validate_email(""))
+    def test_email_is_empty(self):
+        self.assertFalse(UV.validate_email(""))
 
-    def test_email_none(self):
-        self.assertFalse(UserValidation.validate_email(None))
+    def test_email_is_none(self):
+        self.assertFalse(UV.validate_email(None))
 
-    # ---------------- Username Tests ----------------
-    def test_username_valid(self):
-        self.assertTrue(UserValidation.validate_username("ramy_gomaa"))
+    def test_valid_username(self):
+        self.assertTrue(UV.validate_username("ahmed_samir"))
 
-    def test_username_too_short(self):
-        self.assertFalse(UserValidation.validate_username("ab"))
+    def test_username_too_small(self):
+        self.assertFalse(UV.validate_username("aa"))
 
-    def test_username_too_long(self):
-        self.assertFalse(UserValidation.validate_username("ramygomaaisaverylongusername"))
+    def test_username_too_big(self):
+        self.assertFalse(UV.validate_username("ahmedsamirisaverylongusername"))
 
-    def test_username_with_spaces(self):
-        self.assertFalse(UserValidation.validate_username("ramy gomaa"))
+    def test_username_has_spaces(self):
+        self.assertFalse(UV.validate_username("ahmed samir"))
 
-    def test_username_with_symbols(self):
-        self.assertFalse(UserValidation.validate_username("ramy@123"))
+    def test_username_has_special_chars(self):
+        self.assertFalse(UV.validate_username("ahmed@123"))
 
-    def test_username_with_digits(self):
-        self.assertTrue(UserValidation.validate_username("ramy123"))
+    def test_username_with_numbers(self):
+        self.assertTrue(UV.validate_username("ahmed123"))
 
-    def test_username_empty(self):
-        self.assertFalse(UserValidation.validate_username(""))
+    def test_username_is_empty(self):
+        self.assertFalse(UV.validate_username(""))
 
-    def test_username_none(self):
-        self.assertFalse(UserValidation.validate_username(None))
+    def test_username_is_none(self):
+        self.assertFalse(UV.validate_username(None))
 
-    # ---------------- Phone Tests ----------------
-    def test_phone_valid_vodafone(self):
-        self.assertTrue(UserValidation.validate_phone_number("01012345678"))
+    def test_phone_vodafone(self):
+        self.assertTrue(UV.validate_phone_number("01098765432"))
 
-    def test_phone_valid_orange(self):
-        self.assertTrue(UserValidation.validate_phone_number("01234567890"))
+    def test_phone_orange(self):
+        self.assertTrue(UV.validate_phone_number("01234567890"))
 
-    def test_phone_valid_etisalat(self):
-        self.assertTrue(UserValidation.validate_phone_number("01198765432"))
+    def test_phone_etisalat(self):
+        self.assertTrue(UV.validate_phone_number("01123456789"))
 
-    def test_phone_valid_we(self):
-        self.assertTrue(UserValidation.validate_phone_number("01555555555"))
+    def test_phone_we(self):
+        self.assertTrue(UV.validate_phone_number("01599999999"))
 
-    def test_phone_with_country_code_vodafone(self):
-        self.assertTrue(UserValidation.validate_phone_number("201012345678"))
+    def test_phone_vodafone_with_country_code(self):
+        self.assertTrue(UV.validate_phone_number("201098765432"))
 
-    def test_phone_with_country_code_orange(self):
-        self.assertTrue(UserValidation.validate_phone_number("201234567890"))
+    def test_phone_orange_with_country_code(self):
+        self.assertTrue(UV.validate_phone_number("201234567890"))
 
     def test_phone_invalid_prefix(self):
-        self.assertFalse(UserValidation.validate_phone_number("01812345678"))
+        self.assertFalse(UV.validate_phone_number("01811111111"))
 
     def test_phone_too_short(self):
-        self.assertFalse(UserValidation.validate_phone_number("0101234567"))
+        self.assertFalse(UV.validate_phone_number("0101234567"))
 
     def test_phone_too_long(self):
-        self.assertFalse(UserValidation.validate_phone_number("010123456789"))
+        self.assertFalse(UV.validate_phone_number("010123456789"))
 
-    def test_phone_with_characters(self):
-        self.assertFalse(UserValidation.validate_phone_number("01012abc678"))
+    def test_phone_contains_letters(self):
+        self.assertFalse(UV.validate_phone_number("01012abc678"))
 
-    def test_phone_empty(self):
-        self.assertFalse(UserValidation.validate_phone_number(""))
+    def test_phone_is_empty(self):
+        self.assertFalse(UV.validate_phone_number(""))
 
-    def test_phone_none(self):
-        self.assertFalse(UserValidation.validate_phone_number(None))
+    def test_phone_is_none(self):
+        self.assertFalse(UV.validate_phone_number(None))
 
-    # ---------------- National ID Tests ----------------
-    def test_national_id_valid(self):
-        self.assertTrue(UserValidation.validate_national_id("29812251234567"))
+    def test_valid_national_id(self):
+        self.assertTrue(UV.validate_national_id("29812251234567"))
 
-    def test_national_id_too_short(self):
-        self.assertFalse(UserValidation.validate_national_id("2981225123456"))
+    def test_national_id_short(self):
+        self.assertFalse(UV.validate_national_id("2981225123456"))
 
-    def test_national_id_too_long(self):
-        self.assertFalse(UserValidation.validate_national_id("298122512345678"))
+    def test_national_id_long(self):
+        self.assertFalse(UV.validate_national_id("298122512345678"))
 
     def test_national_id_with_letters(self):
-        self.assertFalse(UserValidation.validate_national_id("2981225AB34567"))
+        self.assertFalse(UV.validate_national_id("29812AB1234567"))
 
-    def test_national_id_invalid_century(self):
-        self.assertFalse(UserValidation.validate_national_id("19812251234567"))
+    def test_national_id_wrong_century(self):
+        self.assertFalse(UV.validate_national_id("19812251234567"))
 
-    def test_national_id_invalid_month(self):
-        self.assertFalse(UserValidation.validate_national_id("29813251234567"))
+    def test_national_id_wrong_month(self):
+        self.assertFalse(UV.validate_national_id("29813251234567"))
 
-    def test_national_id_invalid_day(self):
-        self.assertFalse(UserValidation.validate_national_id("29812323234567"))
+    def test_national_id_wrong_day(self):
+        self.assertFalse(UV.validate_national_id("29812323234567"))
 
-    def test_national_id_invalid_governorate(self):
-        self.assertFalse(UserValidation.validate_national_id("29812380034567"))
+    def test_national_id_invalid_gov_code(self):
+        self.assertFalse(UV.validate_national_id("29812380034567"))
 
-    def test_national_id_empty(self):
-        self.assertFalse(UserValidation.validate_national_id(""))
+    def test_national_id_is_empty(self):
+        self.assertFalse(UV.validate_national_id(""))
 
-    def test_national_id_none(self):
-        self.assertFalse(UserValidation.validate_national_id(None))
+    def test_national_id_is_none(self):
+        self.assertFalse(UV.validate_national_id(None))
 
 
 if __name__ == "__main__":
